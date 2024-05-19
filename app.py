66d3767837ded5bfb6eb602e402a484323bc921e
Dinamitrii <k_karaivanov@abv.bg>
2024-05-19 13:44:18 +0300

    from win again grr

--- app.py
+++ app.py
@@ -25,29 +25,10 @@ def contacts():
 def mosaic():
     return render_template("mosaic.html")
 
-<<<<<<< HEAD
-=======
-
->>>>>>> 0cb564ecf3975c5ed6baac3c90f01f4fdd97a3fe
 @app.route("/napkins/")
 
 def napkins():
     return render_template("napkins.html")
 
-<<<<<<< HEAD
-
-@app.route("/kitchen/")
-
-def kitchen():
-    return render_template("kitchen.html")
-
-@app.route("/toipaper/")
-
-def toipaper():
-    return render_template("toipaper.html")
-
-
-=======
->>>>>>> 0cb564ecf3975c5ed6baac3c90f01f4fdd97a3fe
 if __name__ == "__main__":
     serve(app.run()) # type: ignore
