a5869858fcdecab3829235b669aba28db84bd129
dinamitrii <k_karaivanov@abv.bg>
2024-05-21 00:35:03 +0300

    From PyCharm favicon working

--- app.py
+++ app.py
@@ -26,7 +26,7 @@ def napkins():
 # The code below lets the Flask server respond to browser requests for a favicon
 @app.route("/favicon.ico")
 def favicon():
-    return url_for('static/images/favicon', filename='favicon.ico')
+    return url_for('static', filename='images/favicon/favicon.ico')
 
 
 if __name__ == "__main__":
