250f5b9e796fb7c7baa0967d2cc5af1fd615acdb
Dinamitrii <k_karaivanov@abv.bg>
2024-05-22 17:56:13 +0300

    working nice

--- app.py
+++ app.py
@@ -26,7 +26,16 @@ def napkins():
 # The code below lets the Flask server respond to browser requests for a favicon
 @app.route("/favicon.ico")
 def favicon():
-    return url_for('static', filename='images/favicon/favicon.ico'), url_for('static', filename='images/favicon/favicon-16x16.png')
+    return (url_for('static', filename='images/favicon/favicon.ico'),
+            url_for('static', filename='images/favicon/favicon-16x16.png'),
+            url_for('static', filename='images/favicon/favicon-32x32.png'),
+            url_for('static', filename='images/favicon/android-chrome-192x192.png'),
+            url_for('static', filename='images/favicon/android-chrome-256x256.png'),
+            url_for('static', filename='images/favicon/apple-touch-icon.png'),
+            url_for('static', filename='images/favicon/safari-pinned-tab.svg'),
+            url_for('static', filename='images/favicon/mstile-150x150.png'),
+            url_for('static', filename='images/favicon/browserconfig.xml'),
+            url_for('static', filename='images/favicon/site.webmanifest'))
 
 
 @app.route("/rollstp/")
